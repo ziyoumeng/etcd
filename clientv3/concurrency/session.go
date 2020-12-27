d0a14ad36a2cfd2223337f45f10d83d88fe592ee
李枫 <fengli@boyaa.com>
2020-12-27 16:49:10 +0800

    feat:读session

--- clientv3/concurrency/session.go
+++ clientv3/concurrency/session.go
@@ -25,13 +25,14 @@ const defaultSessionTTL = 60
 
 // Session represents a lease kept alive for the lifetime of a client.
 // Fault-tolerant applications may use sessions to reason about liveness.
+// Session为传入的client维持了一个不断刷新的lease,以监听etcd client连接的可用性，并对外暴露了Done()方法，当不可用时作为通知
 type Session struct {
 	client *v3.Client
 	opts   *sessionOptions
 	id     v3.LeaseID
 
 	cancel context.CancelFunc
-	donec  <-chan struct{}
+	donec  <-chan struct{} //存在的作用：对我暴露一个Chan以监听Session的关闭信号
 }
 
 // NewSession gets the leased session for a client.
@@ -49,11 +50,11 @@ func NewSession(client *v3.Client, opts ...SessionOption) (*Session, error) {
 		}
 		id = resp.ID
 	}
-
+	//lease.KeepAlive的经典用法
 	ctx, cancel := context.WithCancel(ops.ctx)
 	keepAlive, err := client.KeepAlive(ctx, id)
 	if err != nil || keepAlive == nil {
-		cancel()
+		cancel() //释放ctx资源
 		return nil, err
 	}
 
@@ -62,8 +63,8 @@ func NewSession(client *v3.Client, opts ...SessionOption) (*Session, error) {
 
 	// keep the lease alive until client error or cancelled context
 	go func() {
-		defer close(donec)
-		for range keepAlive {
+		defer close(donec) // etcd客户端后，做一些业务的善后工作
+		for range keepAlive { // 通过keepAlive ch感知etcd客户端何时关闭
 			// eat messages until keep alive channel closes
 		}
 	}()
@@ -79,15 +80,15 @@ func (s *Session) Client() *v3.Client {
 // Lease is the lease ID for keys bound to the session.
 func (s *Session) Lease() v3.LeaseID { return s.id }
 
-// Done returns a channel that closes when the lease is orphaned, expires, or
+// Done returns a channel that closes when the lease is orphaned(孤儿), expires, or
 // is otherwise no longer being refreshed.
 func (s *Session) Done() <-chan struct{} { return s.donec }
 
 // Orphan ends the refresh for the session lease. This is useful
-// in case the state of the client connection is indeterminate (revoke
+// in case the state of the client connection is indeterminate（不确定的） (revoke
 // would fail) or when transferring lease ownership.
 func (s *Session) Orphan() {
-	s.cancel()
+	s.cancel()//停止刷新 session lease后，keepAlive会被关闭，NewSession中的读携程在结束时会close(s.donec)
 	<-s.donec
 }
 
